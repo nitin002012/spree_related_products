6f25e9ea23407599d6078e8b17dfa6e50e4f3039
Nitin Kabi <nitin.kabi@hyscaler.com>
2024-08-03 16:25:02 +0530

    7

--- lib/spree_related_products/engine.rb
+++ lib/spree_related_products/engine.rb
@@ -1,8 +1,31 @@
 module SpreeRelatedProducts
   class Engine < Rails::Engine
-    initializer "spree_related_products.configure_rails_initialization" do
-      config.autoload_paths ||= []
-      config.autoload_paths << "#{root}/lib"
+    require 'spree/core'
+    isolate_namespace Spree
+    engine_name 'spree_related_products'
+
+    config.autoload_paths ||= []
+    config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/models/spree/calculator)
+
+    initializer 'spree_related_products.add_menu_item' do |app|
+      Spree::Backend::Config.configure do |config|
+        config.menu_items << config.class::MenuItem.new(
+          [:related_products],
+          'project-diagram',
+          url: :related_admin_products_path,
+          condition: -> { can?(:manage, Spree::Product) }
+        )
+      end if defined?(Spree::Backend::Config)
     end
+    class << self
+      def activate
+        cache_klasses = %W(#{config.root}/app/**/*_decorator*.rb)
+        Dir.glob(cache_klasses) do |klass|
+          Rails.configuration.cache_classes ? require(klass) : load(klass)
+        end
+      end
+    end
+
+    config.to_prepare(&method(:activate).to_proc)
   end
 end
